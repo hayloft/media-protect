0ed37abd0950e2ebd50119cd677584e2079a63dc
benn0r <benjamin@benn0r.ch>
2020-12-10 17:42:25 +0100

    Initial commit

---
+++ hl-media-protect.php
@@ -0,0 +1,234 @@
+<?php
+
+/*
+Plugin Name: Media Protect
+Plugin URI: https://www.hayloft-it.ch
+Version: 1.0
+Author: Hayloft-IT GmbH
+Author URI: https://www.hayloft-it.ch
+Text Domain: hl-media-protect
+Description: Schützt Medien vor unberechtigten Zugriffen.
+*/
+
+class HL_Media_Protect
+{
+    public function register()
+    {
+        add_filter('mod_rewrite_rules', function($rules) {
+            return $this->rewrite_rules($rules);
+        });
+
+        add_action('init', function() {
+            if (isset($_GET['hl_download']) && isset($_GET['attachment_id'])) {
+                $this->serve($_GET['attachment_id']);
+            }
+        });
+
+        add_action('the_post', function(WP_Post $post) {
+            if ($post->post_type === 'attachment') {
+                $this->filter_attachment($post);
+            }
+        });
+
+        add_filter('redirect_canonical', function($redirect_url, $requested_url) {
+            return $this->fix_redirect($redirect_url, $requested_url);
+        }, 10, 2);
+
+        add_filter('the_posts', function($posts, WP_Query $query) {
+            return $this->filter_posts($posts, $query);
+        }, 10, 2);
+
+        add_action('edit_attachment', function($post_id) {
+            $this->flush_rewrite_rules();
+        }, 1000, 2);
+
+        $this->register_acf();
+    }
+
+    private function register_acf()
+    {
+        acf_add_local_field_group(array(
+            'key' => 'group_5fd23aef8d9ee',
+            'title' => 'Medien Passwort',
+            'fields' => array(
+                array(
+                    'key' => 'field_5fd23b05c9ecd',
+                    'label' => 'Sichtbarkeit',
+                    'name' => 'hl_visibility',
+                    'type' => 'select',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'choices' => array(
+                        'public' => 'Öffentlich',
+                        'private' => 'Privat',
+                        'password' => 'Passwortgeschützt',
+                    ),
+                    'default_value' => false,
+                    'allow_null' => 0,
+                    'multiple' => 0,
+                    'ui' => 0,
+                    'return_format' => 'value',
+                    'ajax' => 0,
+                    'placeholder' => '',
+                ),
+                array(
+                    'key' => 'field_5fd23b1dc9ece',
+                    'label' => 'Passwort',
+                    'name' => 'hl_password',
+                    'type' => 'text',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => array(
+                        array(
+                            array(
+                                'field' => 'field_5fd23b05c9ecd',
+                                'operator' => '==',
+                                'value' => 'password',
+                            ),
+                        ),
+                    ),
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'default_value' => '',
+                    'placeholder' => '',
+                    'prepend' => '',
+                    'append' => '',
+                    'maxlength' => '',
+                ),
+            ),
+            'location' => array(
+                array(
+                    array(
+                        'param' => 'attachment',
+                        'operator' => '==',
+                        'value' => 'all',
+                    ),
+                ),
+            ),
+            'menu_order' => 0,
+            'position' => 'normal',
+            'style' => 'default',
+            'label_placement' => 'top',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => '',
+            'active' => true,
+            'description' => '',
+        ));
+    }
+
+    private function serve($attachment_id)
+    {
+        $visibility = get_field('hl_visibility', $attachment_id);
+        switch ($visibility) {
+            case 'private':
+                if (current_user_can('read_private_posts')) {
+                    $this->download($attachment_id);
+                }
+                break;
+            case 'password':
+                $attachment = get_post($attachment_id);
+                $attachment->post_password = apply_filters('hl_media_protect_password', get_field('hl_password', $attachment_id));
+
+                if (current_user_can('read_private_posts') || !post_password_required($attachment)) {
+                    $this->download($attachment_id);
+                    break;
+                }
+                if (post_password_required($attachment)) {
+                    remove_filter('the_content', 'prepend_attachment');
+                }
+                break;
+            default:
+                $this->download($attachment_id);
+        }
+    }
+
+    private function download($attachment_id)
+    {
+        header('Content-type: ' . get_post_mime_type($attachment_id));
+        echo file_get_contents(get_attached_file($attachment_id));
+        exit;
+    }
+
+    private function rewrite_rules($rules)
+    {
+        $attachments = get_posts([
+            'post_type' => 'attachment',
+            'post_status' => null,
+            'meta_key' => 'hl_visibility',
+            'meta_value' => ['private', 'password'],
+        ]);
+
+        $files_rules = [];
+        foreach ($attachments as $attachment) {
+            $url = wp_get_attachment_url($attachment->ID);
+            $local_url = explode(site_url(), $url)[1];
+            $local_url = str_replace('/wp-content', 'wp-content', $local_url);
+
+            $files_rules[] = 'RewriteRule ^' . $local_url . '$ /index.php?hl_download=1&attachment_id=' . $attachment->ID . ' [NC,L]';
+        }
+
+        $rules .= '<IfModule mod_rewrite.c>' . PHP_EOL
+            . 'RewriteEngine On' . PHP_EOL
+            . implode(PHP_EOL, $files_rules) . PHP_EOL
+            . '</IfModule>';
+
+        return $rules;
+    }
+
+    private function filter_attachment(WP_Post $post)
+    {
+        $attachment_id = $post->ID;
+        $visibility = get_field('hl_visibility', $attachment_id);
+        if ($visibility === 'password') {
+            $post->post_password = apply_filters('hl_media_protect_password', get_field('hl_password', $attachment_id));
+        }
+    }
+
+    private function filter_posts($posts, WP_Query $query)
+    {
+        if (!count($posts) && isset($_GET['hl_download']) && isset($_GET['attachment_id'])) {
+            $attachment = get_post($_GET['attachment_id']);
+            if ($attachment) {
+                if (get_field('hl_visibility', $attachment) === 'private'
+                    && !current_user_can('read_private_posts')
+                ) {
+                    return $posts;
+                }
+
+                $query->is_404 = false;
+                $query->is_feed = false;
+                return [$attachment];
+            }
+        }
+
+        return $posts;
+    }
+
+    private function fix_redirect($redirect_url, $requested_url)
+    {
+        if (isset($_GET['hl_download']) && isset($_GET['attachment_id'])) {
+            // do not redirect
+            return $requested_url;
+        }
+
+        return $redirect_url;
+    }
+
+    private function flush_rewrite_rules()
+    {
+        save_mod_rewrite_rules();
+    }
+}
+
+if (class_exists(ACF::class)) {
+    (new HL_Media_Protect())->register();
+}
